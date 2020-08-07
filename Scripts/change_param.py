a4caf02f7befa9492463db7ccc7b6709e05d802a
Zhenguang Huang <zghuang@umich.edu>
2020-08-06 23:34:07 -0700

    Cleaned up the code. The user could provide either TIME or AMAP or both. If ONLY TIME is provided, the script would try to download the ADAPT map. If both TIME and AMAP are provided and the time is inconsistent between the two, the script would stop. If ONLY MAP is provided, the script would upate the #STARTTIME based on the map time.

--- Scripts/change_param.py
+++ Scripts/change_param.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-""" Hello world
-"""
 
 import argparse
 import datetime as dt
 import swmfpy
 import sys
 import subprocess
+from remap_magnetogram import FITS_RECOGNIZE
 
 if __name__ == '__main__':
 
@@ -16,64 +15,76 @@ if __name__ == '__main__':
     ARG_PARSER = argparse.ArgumentParser(description=PROG_DESCRIPTION)
     ARG_PARSER.add_argument('-p', '--poynting_flux',
                             help='(default: 1.0e6 J/m^2/s/T)',
-                            type=float,
-                            default=1.e6)
+                            type=float, default=1.e6)
     ARG_PARSER.add_argument('-t', '--time',
-                            help='(default: Read PARAM.in time.)'
-                            + 'Use if you want'
-                            + ' to overwrite PARAM.in time.'
+                            help='(default: None.)'
+                            + 'Use if you want to overwrite PARAM.in time.'
                             + ' Format: yyyy mm dd hh min',
-                            nargs=5,
-                            type=int,
-                            default=None)
+                            nargs=5, type=int, default=None)
     ARG_PARSER.add_argument('-B0', '--potentialfield',
                             help='(default: HARMONICS.)'
-                            + ' Use if you want to specify '
-                            + ' the potential field solver. ',
-                            type=str,
-                            default='HARMONICS')
-    ARG_PARSER.add_argument('-m', '--map',
-                            help='(default: NONE.)'
-                            + ' Use if you want to specify '
-                            + ' the ADAPT map. ',
-                            type=str,
-                            default='NONE')
+                            + ' Use if you want to specify the PFSS solver.',
+                            type=str, default='HARMONICS')
+    ARG_PARSER.add_argument('-m', '--amap',
+                            help='(default: None.)'
+                            + ' Use if you want to specify the ADAPT map.',
+                            type=str, default=None)
     ARGS = ARG_PARSER.parse_args()
 
-    # Set the start time
-    if len(ARGS.time) == 5:  # Argument given
-        TIME = dt.datetime(ARGS.time[0],
-                           ARGS.time[1],
-                           ARGS.time[2],
-                           ARGS.time[3], 
-                           ARGS.time[4])
-        swmfpy.paramin.replace_command({'#STARTTIME': ARGS.time},
-                                       'PARAM.in',
-                                       'PARAM.in')
+    if ARGS.time != None: 
+        if len(ARGS.time) == 5:
+            # TIME is given with the correct format
+            time_input = dt.datetime(ARGS.time[0],
+                                     ARGS.time[1],
+                                     ARGS.time[2],
+                                     ARGS.time[3], 
+                                     ARGS.time[4])
+            time_param = ARGS.time
+        else:
+            raise ValueError(ARGS.time,
+                             'Time sould be given as yyyy mm dd hh min. ' +
+                             'Correct -t/--time')
+
+    if (ARGS.amap == None):
+        if ARGS.time != None:
+            # Download magnetogram if no map is pvoided
+            # default 'fixed', note that the time_input is correctly set.
+            filename_map = swmfpy.web.download_magnetogram_adapt(time_input)[0]
+        else:
+            raise ValueError(ARGS.time, 'Please provide the time by -t/--time')
     else:
-        raise ValueError('Time sould be given as yyyy mm dd hh min. Correct -t/--time')
+        # The ADAPT map is provied
+        filename_map = ARGS.amap
+        
+        map_local  = FITS_RECOGNIZE(ARGS.amap)
+        time_map   = dt.datetime.strptime(map_local[9], "%Y-%m-%dT%H:%M:%S")
 
+        if ARGS.time != None:
+            # Check if the user provides a different time....
+            if (time_map.year  != time_input.year    or
+                time_map.month != time_input.month   or  
+                time_map.day   != time_input.day     or
+                time_map.hour  != time_input.hour):
+                raise ValueError(ARGS.time, 'MAP and TIME are inconsistent. ' +
+                                 'Either set MAP or TIME; or make them '      +
+                                 'consistent')
+        else:
+            # if the user does not provide the time, then set the time based
+            # on the time info from the ADAPT map.
+            time_param = [time_map.year, time_map.month,
+                          time_map.day,  time_map.hour]
 
-    # Poynting Flux
-    CMD_PFLUX = {'#POYNTINGFLUX': [[str(ARGS.poynting_flux),
+    # set #STARTTIME
+    swmfpy.paramin.replace_command({'#STARTTIME': time_param},
+                                   'PARAM.in', 'PARAM.in')
+
+    # set #POYNTINGFLUX
+    str_flux = {'#POYNTINGFLUX': [['{:<10.3e}'.format(ARGS.poynting_flux), 
                                     'PoyntingFluxPerBSi [J/m^2/s/T]']]}
-    swmfpy.paramin.replace_command(CMD_PFLUX,
-                                   'PARAM.in',
-                                   'PARAM.in')
+    swmfpy.paramin.replace_command(str_flux, 'PARAM.in', 'PARAM.in')
 
-    # Download magnetogram and remap if no maps are pvoided
-    if (ARGS.map == 'NONE'):
-        FILE = swmfpy.web.download_magnetogram_adapt(TIME)[0]  # default 'fixed'
-    else:
-        FILE = ARGS.map
-
-    exe_string = str('Scripts/remap_magnetogram.py ' + FILE + ' -istart 1 -iend 12')
-
-    print(subprocess.call(exe_string, shell=True))
-
-    if (ARGS.potentialfield == 'HARMONICS'):
-        print("The default is HARMONICS, no need to change")
-    elif (ARGS.potentialfield == 'FDIPS'):
+    # set the PFSS solver, FDIPS or Harmonics
+    if (ARGS.potentialfield == 'FDIPS'):
         input_file  = open('PARAM.in', 'rt')
         lines = []
         while True:
@@ -98,6 +109,12 @@ if __name__ == '__main__':
 
         input_file.close()
         output_file.close()
-    else:
+    elif ARGS.potentialfield != 'HARMONICS':
         raise ValueError(ARGS.potentialfield + ' must be either HARMONICS or FDIPS')
 
+
+    # prepare each realization map.
+    str_exe = str('Scripts/remap_magnetogram.py ' + filename_map
+                  + ' -istart 1 -iend 12')
+
+    subprocess.call(str_exe, shell=True)
