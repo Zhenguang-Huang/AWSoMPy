30cb61bff0e6c22adf60518f799a5732e18ffbf6
Zhenguang Huang <zghuang@umich.edu>
2020-10-12 02:18:44 -0400

    Improved the script. The user now can set either TIME or MAP or both:
    1. Both TIME and MAP are provided. The script will use the map and
       set the start time based on TIME.
    2. Only TIME is provided. The script will download the ADAPT map and
       set the start time based on the info from the map.
    3. Only MAP is provided. The script will use the map and set the start
       time based on the info from the map.

--- Scripts/change_param.py
+++ Scripts/change_param.py
@@ -8,72 +8,165 @@ import subprocess
 import warnings
 from remap_magnetogram import FITS_RECOGNIZE
 
+# -------------------------------------------------------------------------------
+def add_command(NameCommand, filenameInput='PARAM.in',
+                filenameOut='PARAM.in', NameNextLine=None):
+    """
+    """
+
+    file_input  = open(filenameInput, 'rt')
+
+    lines = []
+    while True:
+        line = file_input.readline()
+        if not line: break
+
+        if NameCommand in line and line[0] != '#':
+            if NameNextLine != None:
+                linenext = file_input.readline()
+                if NameNextLine in linenext:
+                    line = '#'+line
+                lines.append(line)
+                lines.append(linenext)
+            else:
+                line = '#'+line
+                lines.append(line)
+        else:
+            lines.append(line)
+
+    file_input.close()
+
+    file_output = open(filenameOut, 'w')
+    for line in lines:
+        file_output.write(line)
+    file_output.close()
+
+# -------------------------------------------------------------------------------
+def remove_command(NameCommand, filenameInput='PARAM.in',
+                   filenameOut='PARAM.in', NameNextLine=None):
+    """
+    """
+
+    file_input  = open(filenameInput, 'rt')
+
+    lines = []
+    while True:
+        line = file_input.readline()
+        if not line: break
+
+        if NameCommand in line and line[0] == '#':
+            if NameNextLine != None:
+                linenext = file_input.readline()
+                if NameNextLine in linenext:
+                    line = line[1:]
+                lines.append(line)
+                lines.append(linenext)
+            else:
+                lines.append(line[1:])
+        else:
+            lines.append(line)
+
+    file_input.close()
+
+    file_output = open(filenameOut, 'w')
+    for line in lines:
+        file_output.write(line)
+    file_output.close()
+
+# -------------------------------------------------------------------------------
+def change_param_value(NameParam, ValueParam, filenameInput='PARAM.in',
+                       filenameOut='PARAM.in'):
+    """
+    """
+    import re
+
+    file_input  = open(filenameInput, 'rt')
+
+    lines = []
+    while True:
+        line = file_input.readline()
+        if not line: break
+
+        if NameParam in line:
+            print("ValueParam =", ValueParam)
+            print("line =", line)
+            line=re.sub('\d+\.\d+', str(ValueParam), line)
+
+        lines.append(line)
+
+    file_input.close()
+
+    file_output = open(filenameOut, 'w')
+    for line in lines:
+        file_output.write(line)
+    file_output.close()
+
+# ===============================================================================
 if __name__ == '__main__':
 
     # Program initiation
-    PROG_DESCRIPTION = ('Script to automatically download data'
-                        + ' and change PARAM.in if needed')
+    PROG_DESCRIPTION = ('Script to change PARAM.in if needed and '
+                        + ' automatically download the ADAPT map.')
     ARG_PARSER = argparse.ArgumentParser(description=PROG_DESCRIPTION)
     ARG_PARSER.add_argument('-p', '--poynting_flux',
                             help='(default: -1.0 J/m^2/s/T)',
                             type=float, default=-1)
     ARG_PARSER.add_argument('-t', '--time',
-                            help='(default: None.)'
+                            help='(default: NoTime.)'
                             + 'Use if you want to overwrite PARAM.in time.'
-                            + ' Format: yyyy mm dd hh min',
-                            nargs=5, type=int, default=None)
+                            + ' Format: yyyy-mm-ddThh:min:sec',
+                            type=str, default='NoTime')
     ARG_PARSER.add_argument('-B0', '--potentialfield',
                             help='(default: HARMONICS.)'
                             + ' Use if you want to specify the PFSS solver.',
                             type=str, default='HARMONICS')
-    ARG_PARSER.add_argument('-m', '--amap',
-                            help='(default: None.)'
+    ARG_PARSER.add_argument('-m', '--map',
+                            help='(default: NoMap.)'
                             + ' Use if you want to specify the ADAPT map.',
-                            type=str, default=None)
+                            type=str, default='NoMap')
     ARGS = ARG_PARSER.parse_args()
 
-    if ARGS.time != None: 
-        if len(ARGS.time) == 5:
-            # TIME is given with the correct format
-            time_input = dt.datetime(ARGS.time[0],
-                                     ARGS.time[1],
-                                     ARGS.time[2],
-                                     ARGS.time[3], 
-                                     ARGS.time[4])
-            time_param = ARGS.time
-        else:
-            raise ValueError(ARGS.time,
-                             'Time sould be given as yyyy mm dd hh min. ' +
-                             'Correct -t/--time')
+    if ARGS.time != 'NoTime':
+        # TIME is given with the correct format
+        time_input = dt.datetime.strptime(ARGS.time, "%Y-%m-%dT%H:%M:%S")
+        time_param = [[time_input.year,  'iYear'],
+                      [time_input.month, 'iMonth'],
+                      [time_input.day,   'iDay'],
+                      [time_input.hour,  'iHour'],
+                      [time_input.minute,'iMinute'],
+                      [time_input.second,'iSecond']]
 
-    if (ARGS.amap == None):
-        if ARGS.time != None:
-            # Download magnetogram if no map is pvoided
+    if (ARGS.map == 'NoMap'):
+        if ARGS.time != 'NoTime':
+            # Download the ADAPT magnetogram if no map is pvoided
             # default 'fixed', note that the time_input is correctly set.
             filename_map = swmfpy.web.download_magnetogram_adapt(time_input)[0]
+            print("download the map as: ", filename_map)
         else:
-            raise ValueError(ARGS.time, 'Please provide the time by -t/--time')
+            raise ValueError('No map is provided. Please provide the time '
+                             + 'by -t/--time to download the ADAPT map.')
     else:
         # The ADAPT map is provied
-        filename_map = ARGS.amap
+        filename_map = ARGS.map
         
-        map_local  = FITS_RECOGNIZE(ARGS.amap)
+        map_local  = FITS_RECOGNIZE(ARGS.map)
         time_map   = dt.datetime.strptime(map_local[9], "%Y-%m-%dT%H:%M:%S")
 
-        if ARGS.time != None:
-            # Check if the user provides a different time....
-            if (time_map.year  != time_input.year    or
-                time_map.month != time_input.month   or  
-                time_map.day   != time_input.day     or
-                time_map.hour  != time_input.hour):
-                raise ValueError(ARGS.time, 'MAP and TIME are inconsistent. ' +
-                                 'Either set MAP or TIME; or make them '      +
-                                 'consistent')
-        else:
+        # Very weird GONG Synoptic map, the map time is a few days after the end of the CR.
+        # Use an approximation to get the time corresponding to the central meridian
+        if (map_local[0] == 'NSO-GONG Synoptic'):
+            CR_number = float(map_local[6])
+            time_map = dt.datetime(1853, 11, 9) + dt.timedelta(days=27.2753*(CR_number-0.5))
+
+        if ARGS.time == 'NoTime':
             # if the user does not provide the time, then set the time based
             # on the time info from the ADAPT map.
-            time_param = [time_map.year, time_map.month,
-                          time_map.day,  time_map.hour]
+            time_param = [[time_map.year,  'iYear'],
+                          [time_map.month, 'iMonth'],
+                          [time_map.day,   'iDay'],
+                          [time_map.hour,  'iHour'],
+                          [time_map.minute,'iMinute'],
+                          [time_map.second,'iSecond']]
 
     # set #STARTTIME
     swmfpy.paramin.replace_command({'#STARTTIME': time_param},
@@ -90,33 +183,13 @@ if __name__ == '__main__':
 
     # set the PFSS solver, FDIPS or Harmonics
     if (ARGS.potentialfield == 'FDIPS'):
-        input_file  = open('PARAM.in', 'rt')
-        lines = []
-        while True:
-            line = input_file.readline()
-            if not line: break
-
-            if 'LOOKUPTABLE' in line:
-                linenext = input_file.readline()
-                if 'B0' in linenext:
-                    line = '#'+line
-                lines.append(line)
-                lines.append(linenext)
-            elif '#MAGNETOGRAM' in line:
-                line = line[1:]
-                lines.append(line)
-            else:
-                lines.append(line)
-
-        output_file = open('PARAM.in', 'w')
-        for line in lines:
-            output_file.write(line)
-
-        input_file.close()
-        output_file.close()
-    elif ARGS.potentialfield != 'HARMONICS':
+        add_command('LOOKUPTABLE', NameNextLine='B0')
+        remove_command('MAGNETOGRAM')
+    elif (ARGS.potentialfield == 'HARMONICS'):
+        remove_command('LOOKUPTABLE', NameNextLine='B0')
+        add_command('MAGNETOGRAM')
+    else:
         raise ValueError(ARGS.potentialfield + ' must be either HARMONICS or FDIPS')
-
 
     # prepare each realization map.
     str_exe = str('Scripts/remap_magnetogram.py ' + filename_map)
