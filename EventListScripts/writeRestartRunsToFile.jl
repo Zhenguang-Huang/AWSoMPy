68ffcc9f955825e2552b1698cca41f63ceefdfbf
Aniket Jivani <ajivani@umich.edu>
2021-10-02 12:03:27 -0400

    modify script for restart event_list

--- EventListScripts/writeRestartRunsToFile.jl
+++ EventListScripts/writeRestartRunsToFile.jl
@@ -28,16 +28,20 @@ using Dates
 using Distributions
 using DelimitedFiles
 
+# DONE:
+# - Can take in multiple background runs and write run IDs correctly
+# - No params that are in the background repeat for the CME runs
+# - added param = PARAM.in.awsom.cme
+
 # TO DO: 
 # - add ArgParse with options to specify restartdir from the command line? 
-# - remove hardcoding for single background run ! 
 # - options - define variable that lists selected background, then have restart = those runs in sequence, 
 # but restart params repeat. 
 # - how do we write in EEGGL params at top? Take from a separate file and merge ? 
 
 
 # restartdir - directory containing background runs to restart (if linking to existing runs)
-# N/A here
+# N/A here?
 
 # specify mg, cr and md
 mg = "ADAPT"
@@ -45,38 +49,51 @@ cr = 2154
 md = "AWSoM"
 
 # number of background runs - load a csv and count
-# right now going with only 1 run so just going to hardcode that (not sure what values to put for now)
-nBackground = 1
+# nBackground = 1
+
+# load file for background params
+XBackground = DataFrame(CSV.File("./SampleOutputs/restartRunDesignFiles/3params_background_0928.csv"))
+# count number of background runs
+nBackground = size(XBackground, 1) # Going to set it to 20 assuming we just do the runs from the CSV file 
+# Add columns for writing out .fits fileName and model
+insertcols!(XBackground, 1, :map=>string(mg, "_CR", "$(cr)",  ".fits"))
+insertcols!(XBackground, 2, :model=>md)
+
+colNamesBackground = [
+                    "map",
+                    "model",
+                    "BrFactor", 
+                    "PoyntingFluxPerBSi", 
+                    "LperpTimesSqrtBSi"
+                    ]
+
+rename!(XBackground, colNamesBackground)
+
 
 # load file for restart params
-XDesign = DataFrame(CSV.File("/Users/ajivani/Desktop/X_design_CME_proposed_3.csv"))
-deletecols!(XDesign, "Column1")
+XRestart = DataFrame(CSV.File("./SampleOutputs/restartRunDesignFiles/X_design_CME_2021_10_01.csv"))
+deletecols!(XRestart, "Column1")
 
 # convert EEGGL Method from (1, 2) to (Distance, Area) => 1 = Distance, 2 = Area
-EEGGLMethod = replace(XDesign.EEGGLMethod, 1=>"Distance", 2=>"Area")
-select!(XDesign, Not(:EEGGLMethod))
-insertcols!(XDesign, :EEGGLMethod=>EEGGLMethod)
+EEGGLMethod = replace(XRestart.EEGGLMethod, 1=>"Distance", 2=>"Area")
+select!(XRestart, Not(:EEGGLMethod))
+insertcols!(XRestart, :EEGGLMethod=>EEGGLMethod)
 
 
-# Add columns for writing out .fits fileName and model
-insertcols!(XDesign, 1, :map=>string(mg, "_CR", "$(cr)",  ".fits"))
-insertcols!(XDesign, 2, :model=>md)
+colNamesRestart = [
+                "Strength",
+                "Radius",
+                "dOrientationCme",
+                "ApexHeight",
+                "FootptDistance",
+                "Helicity",
+                "EEGGLMethod"
+                ]   # give column names for data frame
 
-colNames = ["map",
-            "model",
-            "Strength",
-            "Radius",
-            "Orientation",
-            "ApexHeight",
-            "FootptDistance",
-            "Helicity",
-            "EEGGLMethod"
-            ]   # give column names for data frame
-
-rename!(XDesign, colNames)
+rename!(XRestart, colNamesRestart)
 
 # count number of restarts
-nRestart = size(XDesign, 1)
+nRestart = size(XRestart, 1)
 
 # start restart numbering from nBackground + 1
 runIDRange = (nBackground + 1):(nBackground + nRestart)
@@ -92,18 +109,19 @@ fileName = "event_list_" * mg * "_" * md * "_" * "CR$(cr)" * "_" * currDTString 
 (tmppath, tmpio) = mktemp()
 
 # for loop for writing out to file
-write(tmpio, "selected run IDs = 1-$(size(XDesign, 1) + nBackground)\n")
+write(tmpio, "selected run IDs = 1-$(size(XRestart, 1) + nBackground)\n")
 write(tmpio, "#START\n")
 write(tmpio, "ID   params\n")
 
-# Loop through DataFrame
-for count = 1:size(XDesign, 1)
-    dfParams = XDesign[count, 3:end]
+# Loop through DataFrame for background
+for count = 1:size(XBackground, 1)
+
+    dfParams = XBackground[count, 3:end]
     dictParams = Dict(names(dfParams) .=> values(dfParams))
-    stringToWrite = ""
-    
-    dfMapModel = XDesign[count, 1:2]
+
+    dfMapModel = XBackground[count, 1:2]                      # By convention, map and model will be the first 2 columns
     dictMapModel = Dict(names(dfMapModel) .=> values(dfMapModel))
+    stringToWrite = ""
 
     # we write map and model in a different loop to ensure they come first in each line of the list. 
     for (key, value) in dictMapModel
@@ -124,7 +142,33 @@ for count = 1:size(XDesign, 1)
         stringToWrite = stringToWrite * appendVal
     end
 
-    write(tmpio, string(runIDRange[count]) * " " * "restart=run001_AWSoM " * stringToWrite * "\n")
+    write(tmpio, 
+        string(count) * " " * stringToWrite * "\n")
+
+end
+
+
+# Loop through DataFrame for restart
+for count = 1:size(XRestart, 1)
+    dfParams = XRestart[count, 1:end]
+    dictParams = Dict(names(dfParams) .=> values(dfParams))
+    stringToWrite = ""
+
+    for (key, value) in dictParams
+        if value isa String
+            appendVal = @sprintf("%s=%s         ", key, value)
+        elseif value isa Array
+            appendVal = @sprintf("%s=[%d]    ", key, value[1])
+        elseif value >= 1000
+            appendVal = @sprintf("%s=%e    ", key, value)
+        else
+            appendVal = @sprintf("%s=%.4f    ", key, value)
+        end
+        stringToWrite = stringToWrite * appendVal
+    end
+
+    write(tmpio, 
+        string(runIDRange[count]) * " " * "restartdir=          " * " param=PARAM.in.awsom.cme      " * stringToWrite * "\n")
 end
 close(tmpio); 
 mv(tmppath, joinpath("./SampleOutputs/", fileName), force=true)
