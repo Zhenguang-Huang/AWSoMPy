550dc1443c43b6d60aaf750f526e233d43758e13
Lulu Zhao <zhlulu@login2.frontera.tacc.utexas.edu>
2022-02-09 22:14:22 -0600

    add AWSoMR_OHSP and fix an error

--- Scripts/sub_runs.py
+++ Scripts/sub_runs.py
@@ -287,8 +287,8 @@ if __name__ == '__main__':
 
             # Compile the code if needed. AWSoM and AWSoM-R could not be 
             # selected at the same time
-            if not MODEL in ['AWSoM','AWSoMR','AWSoM2T']:
-                warnings.warn(MODEL, ' may not be supported.')
+            if not MODEL in ['AWSoM','AWSoMR','AWSoM2T','AWSoMR_OHSP']:
+                warnings.warn(MODEL+' may not be supported.')
 
             # If the corresponding MODEL.exe does not exist, need to re-compile the code.
             # If it exists, do not change ARGS.DoCompile, which default is 1 (to re-compile
